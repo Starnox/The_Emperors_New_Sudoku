0b68cf1a08933d6141f01b6af6682331ca7cf86c
Mihailescu Eduard <mihailescu.eduard@gmail.com>
2021-01-06 21:31:31 +0200

    Finished task 1

--- tema3.c
+++ tema3.c
@@ -1,13 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "cJSON.h"
 #include "tema3.h"
-#include "bmp_header.h"
-
-void ParseFileHeader(cJSON *sudoku ,bmp_fileheader *myFileHeader);
-void ParseInfoHeader(cJSON *sudoku ,bmp_infoheader *myInfoHeader);
-void ParseBitmap(cJSON *sudoku ,Pixel *pixelMatrix);
 
 int main(int argc, char *argv[] )
 {
@@ -29,9 +20,15 @@ int main(int argc, char *argv[] )
 
     // TASK1
     Pixel *pixelMatrix = NULL;
-    ParseJSON(inputString,pixelMatrix);
+    bitmap *myBitmap = NULL;
+    myBitmap = ParseJSON(inputString, &pixelMatrix);
 
+    CreateBMPFromJPEG(myBitmap,pixelMatrix);
 
+    free(myBitmap);
+    free(pixelMatrix);
+    //free(myFileHeader);
+    //free(myInfoheader);
     free(inputString);
     return 0;
 }
@@ -60,12 +57,11 @@ char * ReadJSON(const char *argument)
     return inputString;
 }
 
-void ParseJSON(const char * const inputString, Pixel *pixelMatrix)
+// parse JSON and return a bitmap pointer
+bitmap * ParseJSON(const char * const inputString, Pixel **pixelMatrix)
 {
-    // TODO change NUM_OF_PIXELS with values from the file
-    int status = 0;
-
     // declaring
+    bitmap *myBitmap = (bitmap *) calloc(1,sizeof(bitmap));
     bmp_fileheader *myFileHeader = NULL;
     bmp_infoheader *myInfoheader = NULL;
     // TODO free those two
@@ -79,29 +75,25 @@ void ParseJSON(const char * const inputString, Pixel *pixelMatrix)
         {
             fprintf(stderr, "Error before: %s\n", error_ptr);
         }
-        status = 0;
-        return;
+        return NULL;
     }
 
-    ParseFileHeader(sudoku,myFileHeader);
-    ParseInfoHeader(sudoku,myInfoheader);
-    ParseBitmap(sudoku, pixelMatrix);
-
-
-    free(pixelMatrix);
-    free(myFileHeader);
-    free(myInfoheader);
-    return;
+    myFileHeader = ParseFileHeader(sudoku);
+    myInfoheader = ParseInfoHeader(sudoku);
+    *pixelMatrix = ParseBitmap(sudoku, myInfoheader->width, myInfoheader->height);
+    myBitmap->fileheader = *myFileHeader;
+    myBitmap->infoheader = *myInfoheader;
+    return myBitmap;
 
 }
 
-void ParseFileHeader(cJSON *sudoku ,bmp_fileheader *myFileHeader)
+bmp_fileheader * ParseFileHeader(cJSON *sudoku)
 {
-    myFileHeader = (bmp_fileheader *) calloc(1,sizeof(bmp_fileheader));
+    bmp_fileheader *myFileHeader = (bmp_fileheader *) calloc(1,sizeof(bmp_fileheader));
     if(myFileHeader == NULL)
-        return;
+        return NULL;
     // variables for file_headers
-    const cJSON  *file_header, *signature = NULL, *offset = NULL, *reserved = NULL, *file_size = NULL;
+    const cJSON  *file_header, *signature = NULL, *offset = NULL, *file_size = NULL;
     
     // parse the file_header object
     file_header = cJSON_GetObjectItemCaseSensitive(sudoku, "file_header");
@@ -111,57 +103,99 @@ void ParseFileHeader(cJSON *sudoku ,bmp_fileheader *myFileHeader)
         signature = cJSON_GetObjectItemCaseSensitive(file_header,"signature");
         file_size = cJSON_GetObjectItemCaseSensitive(file_header,"file_size");
 
-        if(cJSON_IsNumber(offset) && (offset->valueint != NULL))
-        {
+        if(cJSON_IsNumber(offset))
             myFileHeader->imageDataOffset = offset->valueint;
-            printf("%d\n",myFileHeader->imageDataOffset);
-        }
-        if(cJSON_IsString(signature) && (signature->valuestring != NULL))
+
+        if(cJSON_IsString(signature))
         {
-            printf("%s\n", signature->valuestring);
             myFileHeader->fileMarker1 = signature->valuestring[0];
             myFileHeader->fileMarker2 = signature->valuestring[1];
         }
-        if(cJSON_IsNumber(file_size) && (file_size->valueint != NULL))
+        if(cJSON_IsNumber(file_size))
         {
             myFileHeader->bfSize = file_size->valueint;
-            printf("%d\n",myFileHeader->bfSize);
         }
         myFileHeader->unused1 = 0;
         myFileHeader->unused2 = 0;
         
     }
+    return myFileHeader;
 }
 
-void ParseInfoHeader(cJSON *sudoku ,bmp_infoheader *myInfoHeader)
+bmp_infoheader * ParseInfoHeader(cJSON *sudoku)
 {
     bmp_infoheader *myInfoheader = (bmp_infoheader *) calloc(1,sizeof(bmp_infoheader));
 
     if(myInfoheader == NULL)
-        return ;
+        return NULL;
 
-    const cJSON *info_header, *colors_used, *size, *colors_important, *image_size,
+    const cJSON *info_header, *size,*colors_used , *colors_important, *image_size,
                 *y_pixels_per_meter, *x_pixels_per_meter, *width, *planes, *bit_count,
                 *compression, *height;
     // parse the info_header object
     info_header = cJSON_GetObjectItemCaseSensitive(sudoku, "info_header");
     if(cJSON_IsObject(info_header))
     {
+        size = cJSON_GetObjectItemCaseSensitive(info_header,"size");
+        width = cJSON_GetObjectItemCaseSensitive(info_header,"width");
+        height = cJSON_GetObjectItemCaseSensitive(info_header,"height");
+        planes = cJSON_GetObjectItemCaseSensitive(info_header,"planes");
+        bit_count = cJSON_GetObjectItemCaseSensitive(info_header,"bit_count");
+        compression = cJSON_GetObjectItemCaseSensitive(info_header,"compression");
+        image_size = cJSON_GetObjectItemCaseSensitive(info_header,"image_size");
+        x_pixels_per_meter = cJSON_GetObjectItemCaseSensitive(info_header,"x_pixels_per_meter");
+        y_pixels_per_meter = cJSON_GetObjectItemCaseSensitive(info_header,"y_pixels_per_meter");
+        colors_used = cJSON_GetObjectItemCaseSensitive(info_header,"colors_used");
+        colors_important = cJSON_GetObjectItemCaseSensitive(info_header,"colors_important");
+
+        if(cJSON_IsNumber(size))
+            myInfoheader->biSize = size->valueint;
+
+        if(cJSON_IsNumber(width))
+            myInfoheader->width = width->valueint;
+
+        if(cJSON_IsNumber(height))
+            myInfoheader->height = height->valueint;
+
+        if(cJSON_IsNumber(planes))
+            myInfoheader->planes = planes->valueint;
+
+        if(cJSON_IsNumber(bit_count))
+            myInfoheader->bitPix = bit_count->valueint;
+
+        if(cJSON_IsNumber(compression))
+            myInfoheader->biCompression = compression->valueint;
+
+        if(cJSON_IsNumber(image_size))
+            myInfoheader->biSizeImage = image_size->valueint;
+
+        if(cJSON_IsNumber(x_pixels_per_meter))
+            myInfoheader->biXPelsPerMeter = x_pixels_per_meter->valueint;
+
+        if(cJSON_IsNumber(y_pixels_per_meter))
+            myInfoheader->biYPelsPerMeter = y_pixels_per_meter->valueint;
+
+        if(cJSON_IsNumber(colors_used))
+            myInfoheader->biClrUsed = colors_used->valueint;
+ 
+        if(cJSON_IsNumber(colors_important))
+            myInfoheader->biClrImportant = colors_important->valueint;
 
     }
+    return myInfoheader;
 }
 
-void ParseBitmap(cJSON *sudoku ,Pixel *pixelMatrix)
+Pixel * ParseBitmap(cJSON *sudoku, int width, int height)
 {
      // allocating memory for the pixelMatrix
-    pixelMatrix = (Pixel *) malloc(NUM_OF_PIXELS * NUM_OF_PIXELS * sizeof(Pixel));
+    Pixel *pixelMatrix = (Pixel *) malloc(width * height * sizeof(Pixel));
     // TODO free the pixelMatrix
     if(pixelMatrix == NULL)
-        return;
+        return NULL;
 
     const cJSON *bitmap, *bit;
     bitmap = cJSON_GetObjectItemCaseSensitive(sudoku, "bitmap");
-    int k = 0, i = 72, j = 0, curr_index;
+    int k = 0, i = height-1, j = 0, curr_index;
 
     // iterating through every number, create the pixel and add it from the buttom up
     cJSON_ArrayForEach(bit,bitmap)
@@ -169,11 +203,11 @@ void ParseBitmap(cJSON *sudoku ,Pixel *pixelMatrix)
         if(!cJSON_IsNumber(bit))
         {
             printf("Error");
-            return;
+            return NULL;
         }
         else
         {
-            curr_index = i * NUM_OF_PIXELS + j;
+            curr_index = i * height + j;
             if(k == 0)
                 pixelMatrix[curr_index].b = bit->valueint;
             else if(k == 1)
@@ -186,7 +220,7 @@ void ParseBitmap(cJSON *sudoku ,Pixel *pixelMatrix)
                 k=0;
                 j++;
             }
-            if(j == NUM_OF_PIXELS)
+            if(j == width)
             {
                 i--;
                 j=0;
@@ -194,16 +228,79 @@ void ParseBitmap(cJSON *sudoku ,Pixel *pixelMatrix)
         }
     }
     
-    
     /*
     // printing the pixel array
-    for(i = 0; i< NUM_OF_PIXELS; ++i)
+    for(i = 0; i< height; ++i)
     {
-        for(j = 0; j < NUM_OF_PIXELS; ++j)
+        for(j = 0; j < width; ++j)
         {
-            curr_index = i * NUM_OF_PIXELS + j;
+            curr_index = i * height + j;
             printf("%d %d %d\n", pixelMatrix[curr_index].r, pixelMatrix[curr_index].g, pixelMatrix[curr_index].b);
         }
     }
     */
+    return pixelMatrix;
+}
+
+void CreateBMPFromJPEG(bitmap *myBitmap, Pixel *pixelMatrix)
+{
+    int width = myBitmap->infoheader.width, height = myBitmap->infoheader.height;
+    unsigned char *img = malloc(3 * width * height);
+    unsigned char bmppad[3] = {0, 0, 0};
+    FILE *fp = fopen("test.bmp", "wb");
+    
+    fwrite(myBitmap, 1, sizeof(bitmap), fp);
+    
+    
+    for(int i=0; i< height; ++i)
+    {
+        for(int j=0; j< width; ++j)
+        {
+            int x = i, y = (height - 1) - j;
+            int curr_index = i * height  + j;
+            int r = pixelMatrix[curr_index].r;
+            int g = pixelMatrix[curr_index].g;
+            int b = pixelMatrix[curr_index].b;
+
+            img[i * width * 3 + j * 3 + 2] = (unsigned char) (r);
+            img[i * width * 3 + j * 3 + 1] = (unsigned char) (g);
+            img[i * width * 3 + j * 3 +  0] = (unsigned char) (b);
+            /*
+            img[(x+width * y)*3 + 2] = (unsigned char) (r);
+            img[(x+width * y)*3 + 1] = (unsigned char) (g);
+            img[(x+width * y)*3 + 0] = (unsigned char) (b);
+            */
+        }
+    }
+    
+    for(int i=0; i<height; ++i)
+    {
+        
+        //fwrite(img + (width * (height-i-1)*3), 3, width, fp);
+        fwrite(img + (i * 3 * width), 3, width, fp);
+        fwrite(bmppad, 1, (4-(width * 3)%4)%4, fp); // Padding for each row
+    }
+    
+    
+    /*
+    int bytesPerPixel = sizeof(Pixel);
+    int paddedRowSize = (int)(4 * ceil((float)width/4.0f))*bytesPerPixel;
+    int unpaddedRowSize = width * bytesPerPixel;
+    for(int i = 0; i < height; ++i)
+    {
+        int pixelOffset = ((height - i) - 1)*unpaddedRowSize;
+        fwrite(&pixelMatrix[pixelOffset], 1, paddedRowSize, fp);
+    }
+    */
+
+    /*
+    int i, curr_index,aux;
+    for(i = 0; i< height; ++i)
+    {
+        aux = fwrite(pixelMatrix + (i * 3 * width), sizeof(Pixel), width, fp);
+        //fwrite(bmppad, 1, 1, fp);
+        aux = fwrite(bmppad, 1, (4-(width * 3)%4)%4, fp); // Padding for each row
+    }*/
+    
+    fclose(fp);
 }
--- tema3.h
+++ tema3.h
@@ -1,12 +1,32 @@
-#define NUM_OF_PIXELS 73
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <math.h>
+#include "bmp_header.h"
+#include "cJSON.h"
+
+#pragma pack(1)
 
 typedef struct
 {
-    int r,g,b;
+    unsigned char b;
+    unsigned char g;
+    unsigned char r;
 }Pixel;
 
+typedef struct 
+{
+    bmp_fileheader fileheader;
+    bmp_infoheader infoheader;
+} bitmap;
 
+#pragma pack()
 
 char * ReadJSON(const char *argument);
-void ParseJSON(const char * const inputString, Pixel *pixelMatrix);
+bitmap * ParseJSON(const char * const inputString, Pixel **pixelMatrix);
+bmp_fileheader * ParseFileHeader(cJSON *sudoku);
+bmp_infoheader * ParseInfoHeader(cJSON *sudoku);
+Pixel * ParseBitmap(cJSON *sudoku, int width, int height);
+void CreateBMPFromJPEG(bitmap *myBitmap, Pixel *pixelMatrix);
 
